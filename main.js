a6eab80bb1d21bf82edac80c299eeb1c7ff1d152
Chi Señires <chi.senires@gmail.com>
2016-03-26 13:55:21 +0800

    Updated redirect link from main index page

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ $(function () {
   $('.toggle > input[type="submit"]').click(function () {
     if ($('#user-toggle').find(":selected").text() == 'Student') {
       // If Student Login
-      window.location.href= 'student/login.html';
+      window.location.href= 'student/index.html';
     } else {
       // If Guidance Login
       window.location.href = 'guidance/dashboard.html';
-    }
+    } 
   });
 
   $('#new-student').submit(function (e) {
