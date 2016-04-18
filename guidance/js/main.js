2b77fb72082eaf0af2c8b939de4fe3288dfff739
Chi Señires <chi.senires@gmail.com>
2016-04-18 14:54:21 +0800

    Set up preview of email upon check of message

--- guidance/js/main.js
+++ guidance/js/main.js
@@ -1,5 +1,24 @@
 var emails = document.getElementsByClassName("email");
 
 for (var i = 0; i < emails.length; i++) {
-  console.log(emails[i])
+  emails[i].addEventListener('click', function (e) {
+    // get data
+    var sender = this.children[0].innerText;
+    
+    var emailDetails = this.children[1].getElementsByTagName("span");
+    var emailSubject = emailDetails[0].innerText;
+    var emailPreview = emailDetails[1].innerText;
+
+    var date = this.children[2].innerText;
+
+    // set data to preview
+    var previewBox = document.getElementById("show-message");
+    previewBox.style.display = 'block';
+    previewBox.getElementsByClassName("subject")[0].innerText = emailSubject;
+    var emailForm = document.getElementById("email-message");
+    emailForm.children[0].value = sender;
+    emailForm.children[1].value = date;
+    emailForm.children[2].value = 'iamhamilton@dsad.ph (you)';
+    emailForm.children[3].innerText = emailPreview;
+  });
 }
